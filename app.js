61fe50cb1d0fc324de1004f45fb5313a29139ded
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-09-07 21:30:42 +0530

    create

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const body = require('body-parser');
 const ejs = require('ejs');
 const https = require('https');
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 const app = express();
+const fnameArray=[];
+const lnameArray=[];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(body.urlencoded({ extended: true }));
 
 // database connection
 let connection = mysql.createConnection({
@@ -19,30 +21,38 @@ let connection = mysql.createConnection({
 })
 
 app.get('/', function(req, res){
-
-    
     res.render('home')
 });
 
-app.post('/create', function(req, res){
-    connection.connect(function(){
-        if(err)throw err;
-        console.log("Connected");
-    })
-})
-
 app.get('/create', function(req, res){
-    res.render('create')
-})
+    res.render('create');
+});
 
-app.get('/Update', function(req, res){
-    res.render('update')
-})
+app.post('/create', function(req, res){
+    fnameArray.push(req.body.fname);
+    lnameArray.push(req.body.lname);
+    res.render('database', {
+        fnameArray,
+        lnameArray
+    });
+    console.log(fnameArray);
+    console.log(lnameArray);
+});
 
-app.get('/delete', function(req, res){
-    res.render('delete')
-})
+
+// app.get('/database', function(req, res){
+//     res.render('database')
+// })
 
 app.listen('3000', function(){
     console.log("Server is running on port 3000")
 })
+
+
+// app.get('/Update', function(req, res){
+//     res.render('update')
+// })
+
+// app.get('/delete', function(req, res){
+//     res.render('delete')
+// })
